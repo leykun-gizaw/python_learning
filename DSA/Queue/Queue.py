d1d5e8e62e93ad2f8a3fb81d922db0eb5d204569
Leykun Gizaw <leykungizaw@gmail.com>
2023-11-21 14:33:38 +0300

    fix(DSA): Make packages out of each DS

---
+++ DSA/Queue/Queue.py
@@ -0,0 +1,54 @@
+"""Module defines a Queue class"""
+
+from ..LinkedList.DLList import DLList
+
+class Queue:
+    def __init__(self):
+        self.items = DLList()
+        pass
+
+    def is_empty(self):
+        """Determine if Queue is empty"""
+        return self.items.is_empty()
+
+    def front(self):
+        """Get the front of the queue"""
+        return self.items.get_head()
+
+    def rear(self):
+        """Get the rear of the queue"""
+        return self.items.get_tail()
+
+    def enqueue(self, element):
+        """Enqueue an element into the queue DS
+
+        Args:
+            element (Any): Element to enqueue
+
+        Returns:
+            Enqueued element
+        """
+        return self.items.insert_at_tail(element)
+
+    def dequeue(self):
+        """Dequeue an element from the queue DS
+
+        Returns:
+            Dequeued element
+        """
+        return self.items.remove_at_head()
+
+    def __str__(self):
+        """Print the queue structure"""
+        return self.items.__str__()
+    pass
+
+
+if __name__ == "__main__":
+    queue = Queue()
+
+    for num in range(1, 11):
+        queue.enqueue(num)
+
+    print(queue)
+
--- DSA/Queue/main.py
+++
@@ -1,6 +0,0 @@
-class MyQueue:
-    def __init__(self):
-        self.items = DoublyLinkedList()
-
-    def is_empty(self):
-        return self.items.is_empty()
