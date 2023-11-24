7395fafdec992f77bba48cfb81bc4faa47c0d1ba
Leykun Gizaw <leykungizaw@gmail.com>
2023-11-24 10:58:49 +0300

    fix(Stack): Change the printing method to show hierarchy

--- DS/Stack/Stack.py
+++ DS/Stack/Stack.py
@@ -2,6 +2,7 @@ class Stack:
     def __init__(self):
         self.stack_list = []
         self.stack_size = 0
+        self.max_elem_length = 0
         return
 
     def is_empty(self):
@@ -18,12 +19,18 @@ class Stack:
     def push(self, value):
         self.stack_list.append(value)
         self.stack_size += 1
+        if self.max_elem_length < len(str(value)):
+            self.max_elem_length = len(str(value))
 
     def pop(self):
         if self.is_empty() is not True:
             self.stack_size -= 1
             return self.stack_list.pop()
+        self.max_elem_length = max(len(str(elem)) for elem in self.stack_list)
         return None
 
     def __str__(self):
-        return str(self.stack_list)
+        val = ""
+        for elem in reversed(self.stack_list):
+            print("|", str(elem).center(self.max_elem_length), "|")
+        return val
