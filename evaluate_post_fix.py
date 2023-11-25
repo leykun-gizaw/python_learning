4a1daf92692353c2857682e218e9186561192f89
Leykun Gizaw <leykungizaw@gmail.com>
2023-11-25 11:58:47 +0300

    feat(evaluate_post_fix): Add another which uses `eval`

--- evaluate_post_fix.py
+++ evaluate_post_fix.py
@@ -28,8 +28,24 @@ def evaluate(x: int or float, y: int or float, operator: str) -> int or float:
     res = getattr(x, operator)(y)
     return getattr(x, operator)(y)
 
+def evaluate_post_fix2(exp: str):
+    stack = Stack()
+    
+    try:
+        for char in exp:
+            if char.isdigit():
+                stack.push(char)
+            else:
+                first = stack.pop()
+                second = stack.pop()
+
+                stack.push(str(eval(second + char + first)))
+        return stack.pop()
+    except:
+        return "Invalid Sequence"
+
 
 if __name__ == "__main__":
-    print(evaluate_post_fix("921*-8-4+"))
+    print(evaluate_post_fix2("921*-8-4+"))
     print()
-    print(evaluate_post_fix("642/+"))
+    print(evaluate_post_fix2("642/+"))
