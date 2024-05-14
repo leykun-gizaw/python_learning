3f4e4e49d8440c9e6981e2b35517b0008619877a
Leykun Gizaw <leykungizaw@gmail.com>
2024-05-14 17:21:00 +0300

    refactor(next_greater_element): Add docstring and a bruteforce algo

--- next_greater_element.py
+++ next_greater_element.py
@@ -1,7 +1,53 @@
+# Create a new list with highest elements seen from the right
+#
+#   Eg. for a list lst = [10, 5, 2, 13, 15, 9, 2, 16]
+#   the newly created list will be [13, 13, 13, 15, 16, 16, 16, -1]
+#
+#   Elements in the new list will be replacements of elements in the same index of the original list. Only this time
+#   they will hold values of the first encountered highest element when traversing to the right.
+
 from DS.Stack.Stack import Stack
 
 
-def next_greater_element(lst: list) -> list:
+def next_greater_element1(lst: list) -> list:
+    """Get the result using bruteforce approach
+
+    Complexity analysis:
+        Time: O(n^2) b/c the actual number of cycles is n(n+1)/2
+        Space: O(n)
+
+    Args:
+        lst: List to analyze
+
+    Returns:
+        Next greater elements list
+    """
+    result = list()
+
+    for i in range(len(lst)):
+        next_greatest = -1
+        for j in range(i, len(lst)):
+            if lst[j] > lst[i]:
+                next_greatest = lst[j]
+                break
+        result.append(next_greatest)
+
+    return result
+
+
+def next_greater_element2(lst: list) -> list:
+    """Use a decreasing monotonic stack to reduce time by using some space
+
+    Complexity analysis:
+        Time: O(n) b/c in the worst case the stack will store n - 1 elements that get poped at once.
+        Space: O(n) b/c in any case we create a new list with the exact size.
+
+    Args:
+        lst: List to analyze
+
+    Returns:
+        Next greater elements list
+        """
     my_stack = Stack()
     result = [-1] * len(lst)
 
@@ -18,18 +64,37 @@ def next_greater_element(lst: list) -> list:
             my_stack.pop()
 
         my_stack.push(lst[idx])
-        
+
+    return result
+
+
+def next_greater_element3(lst: list) -> list:
+    """Use stack but start from the beginning
+
+    Complexity analysis: Similary to above
+
+    Args:
+        lst: List to analyze
+
+    Returns:
+        Next greater elements list
+    """
+
+    stack = Stack()
+    result = [-1] * len(lst)
+
+    for idx in range(len(lst)):
+        while stack.is_empty() is False and stack.peek()[1] < lst[idx]:
+            val = stack.pop()
+            result[val[0]] = lst[idx]
+        stack.push([idx, lst[idx]])
+
     return result
 
 
 if __name__ == "__main__":
-    def test(lst):
-        print(lst)
-        result = next_greater_element(lst)
-        print(result)
+    nums1 = [4, 6, 3, 2, 8, 1]
 
-    lst1 = [4, 6, 3, 2, 8, 1]
-    lst2 = [4, 6, 3, 2, 8, 1, 9, 9, 9]
-
-    test(lst1)
-    test(lst2)
+    print(next_greater_element1(nums1))
+    print(next_greater_element2(nums1))
+    print(next_greater_element3(nums1))
