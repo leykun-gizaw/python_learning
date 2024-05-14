2f5b59a1ff7c07a54bacc704446226122b329b22
Leykun Gizaw <leykungizaw@gmail.com>
2024-05-14 15:25:29 +0300

    refactor(post_fix): Add discribing docstrings

--- evaluate_post_fix.py
+++ evaluate_post_fix.py
@@ -2,6 +2,24 @@ from DS.Stack.Stack import Stack
 
 
 def evaluate_post_fix(exp: str):
+    """Evaluate post-fix calculations
+
+    This function uses a hard-coded operators along with built-in methods that represent them.
+    The algorithm works by first going through `exp` and for each character it finds, it tries to search its
+    corresponding method. Once found, it evaluates the proper expression and pushes it back to the stack.
+
+    Complexity analysis:
+        Time: O(n)
+            *Explanation: For any size of `exp` the inner loop in the worst case loops only 5 times.*
+        Space: O(n)
+            *Explanation: In the worst case we only have digits in `exp`.
+
+    Args:
+        exp: Post-fix string to evaluate
+
+    Returns:
+        Result of operation
+    """
     stack = Stack()
     operators = [
         ["+", "__add__"],
@@ -18,7 +36,10 @@ def evaluate_post_fix(exp: str):
                 op_found = True
                 first = stack.pop()
                 second = stack.pop()
-                stack.push(int(evaluate(second, first, op[1])))
+                try:
+                    stack.push(int(evaluate(second, first, op[1])))
+                except AttributeError:
+                    return "Invalid Sequence"
                 break
         if op_found is False:
             stack.push(int(char))
@@ -27,25 +48,41 @@ def evaluate_post_fix(exp: str):
 
 
 def evaluate(x: int or float, y: int or float, operator: str) -> int or float:
-    res = getattr(x, operator)(y)
     return getattr(x, operator)(y)
 
 
 def evaluate_post_fix2(exp: str):
-    stack = Stack()
-    
-    # try:
-    for char in exp:
-        if char.isdigit():
-            stack.push(char)
-        else:
-            first = stack.pop()
-            second = stack.pop()
+    """Evaluate post-fix calculations using `eval`
 
-            stack.push(str(eval(second + char + first)))
-    return stack.pop()
-    # except:
-    #     return "Invalid Sequence"
+    This function uses `eval` instead of going through a custom list of functions to use.
+
+    Complexity analysis:
+        Time: O(n)
+            Explanation: In any case it is important to go through the whole string.
+        Space: O(n)
+            Explanation: In the worst case we only have digits in `exp`. So the stack size will roughly be the same
+            as number of digits in `exp`.
+
+    Args:
+        exp: Post-fix string to evaluate
+
+    Returns:
+        Result of operation
+    """
+    stack = Stack()
+
+    try:
+        for char in exp:
+            if char.isdigit():
+                stack.push(char)
+            else:
+                first = stack.pop()
+                second = stack.pop()
+
+                stack.push(str(eval(second + char + first)))
+        return stack.pop()
+    except TypeError:
+        return "Invalid Sequence"
 
 
 if __name__ == "__main__":
